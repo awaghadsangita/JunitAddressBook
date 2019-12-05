7f2472b1e3cb0500ca6c4b9debdd84e330af1d1f
Admin <you@example.com>
2019-12-05 13:52:44 +0530

    ADD:validate all person information

--- src/main/java/com/addressbook/AddressbookImplementation.java
+++ src/main/java/com/addressbook/AddressbookImplementation.java
@@ -6,36 +6,67 @@ import org.codehaus.jackson.type.TypeReference;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class AddressbookImplementation {
     private List<Person> personList;
     private String fileName;
     ObjectMapper objectmapper = new ObjectMapper();
-    public int addPerson(String fname, String lname, String address, String city, String state, String zip, String ph) throws IOException {
+
+    public Object addPerson(String fname, String lname, String address, String city, String state, String zip, String ph) throws Exception {
+        try {
+            System.out.println(Pattern.compile("^[A-Z]{1}[a-zA-Z]{1,20}$").matcher(fname).matches());
+            if (!Pattern.compile("^[A-Z]{1}[a-zA-Z]{1,20}$").matcher(fname).matches()) {
+                throw new Exception("firstname must start with Uppercase and contain minimum two letters");
+            }
+
+            if (!Pattern.compile("^[A-Z]{1}[a-zA-Z]{1,20}$").matcher(lname).matches()) {
+                throw new Exception("lastname must start with Uppercase and contain minimum two letters");
+            }
+            if (!Pattern.compile("^[a-zA-Z0-9,: -]{2,50}$").matcher(address).matches()) {
+                throw new Exception("address must be minimum two letters long");
+            }
+            if (!Pattern.compile("^[a-zA-Z]{2,50}$").matcher(city).matches()) {
+                throw new Exception("city must be minimum two letters long and Should contain letters only");
+            }
+            if (!Pattern.compile("^[a-zA-Z]{2,50}$").matcher(state).matches()) {
+                throw new Exception("state must be minimum two letters long and Should contain letters only");
+            }
+            if (!Pattern.compile("^[0-9]{6}$").matcher(zip).matches()) {
+                throw new Exception("zip must be 6 digit long");
+            }
+            if (!Pattern.compile("^[0-9]{10}$").matcher(ph).matches()) {
+                throw new Exception("mobile number must be 10 digit long");
+            }
             Person personObj = new Person();
-            personObj.setPersonId(personList.get(personList.size()-1).getPersonId()+1);
+            personObj.setPersonId(personList.get(personList.size() - 1).getPersonId() + 1);
             personObj.setFirstName(fname);
             personObj.setLastName(lname);
-            Address addressObj=new Address();
+            Address addressObj = new Address();
             addressObj.setAddress(address);
             addressObj.setCity(city);
             addressObj.setState(state);
             addressObj.setZip(zip);
             personObj.setAddress(addressObj);
             personObj.setMobile(ph);
-            personList.add(personObj);
+            this.personList.add(personObj);
 
-            writeToJsonFile(personList);
+            writeToJsonFile(this.personList);
             return 1;
+        } catch (Exception e) {
+            return e;
         }
+    }
 
-        public void writeToJsonFile(List<Person> personList) throws IOException {
-            objectmapper.writeValue(new File(this.fileName),personList);
-        }
-        public void readAddressBook(String filename) throws IOException {
-            this.fileName=filename;
-            this.personList = objectmapper.readValue(new File(this.fileName), new TypeReference<List<Person>>(){
-            });
-        }
+
+    public void writeToJsonFile(List<Person> personList) throws IOException {
+        objectmapper.writeValue(new File(this.fileName), personList);
+    }
+
+    public void readAddressBook(String filename) throws IOException {
+        this.fileName = filename;
+        this.personList = objectmapper.readValue(new File(this.fileName), new TypeReference<List<Person>>() {
+        });
+    }
 
 }
--- src/test/java/com/addressbook/testCasesAddressBook.java
+++ src/test/java/com/addressbook/testCasesAddressBook.java
@@ -3,14 +3,54 @@ package com.addressbook;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.IOException;
-
 public class testCasesAddressBook {
     @Test
-    public void givenPersonInformation_InProperFormat_ShouldReturnOne() throws IOException {
+    public void givenPersonInformation_InProperFormat_ShouldReturnOne() throws Exception {
         AddressbookImplementation obj = new AddressbookImplementation();
         obj.readAddressBook("/home/admin1/IdeaProjects/AddressBook/src/main/resources/MH.json");
-        int result=obj.addPerson("sangita","awaghad","Madhuban Colony,Old Jalna","Jalna","MH","431203","9422329006");
-        Assert.assertEquals(1,result);
+        int result = (Integer) obj.addPerson("Sangita", "Awaghad", "Madhuban Colony,Old Jalna", "Jalna", "MH", "431203", "9422329006");
+        Assert.assertEquals(1, result);
+    }
+
+    @Test
+    public void givenPersonInformation_WithFirstNameDontFollowRules_ShouldThrowException() throws Exception {
+        AddressbookImplementation obj = new AddressbookImplementation();
+        Object result = null;
+        obj.readAddressBook("/home/admin1/IdeaProjects/AddressBook/src/main/resources/MH.json");
+        result = obj.addPerson("s", "Awaghad", "Madhuban Colony,Old Jalna", "Jalna", "MH", "431203", "9422329006");
+        Assert.assertEquals("java.lang.Exception: firstname must start with Uppercase and contain minimum two letters", result.toString());
+
+    }
+
+    @Test
+    public void givenPersonInformation_WithCityDontFollowRules_ShouldThrowException() throws Exception {
+        AddressbookImplementation obj = new AddressbookImplementation();
+        obj.readAddressBook("/home/admin1/IdeaProjects/AddressBook/src/main/resources/MH.json");
+        Object result = obj.addPerson("Sangita", "Awaghad", "Madhuban Colony,Old Jalna", "Jalna11", "MH", "431203", "9422329006");
+        Assert.assertEquals("java.lang.Exception: city must be minimum two letters long and Should contain letters only", result.toString());
+    }
+
+    @Test
+    public void givenPersonInformation_WithStateDontFollowRules_ShouldThrowException() throws Exception {
+        AddressbookImplementation obj = new AddressbookImplementation();
+        obj.readAddressBook("/home/admin1/IdeaProjects/AddressBook/src/main/resources/MH.json");
+        Object result = obj.addPerson("Sangita", "Awaghad", "Madhuban Colony,Old Jalna", "Jalna", "MH11", "431203", "9422329006");
+        Assert.assertEquals("java.lang.Exception: state must be minimum two letters long and Should contain letters only", result.toString());
+    }
+
+    @Test
+    public void givenPersonInformation_WithZipDontFollowRules_ShouldThrowException() throws Exception {
+        AddressbookImplementation obj = new AddressbookImplementation();
+        obj.readAddressBook("/home/admin1/IdeaProjects/AddressBook/src/main/resources/MH.json");
+        Object result = obj.addPerson("Sangita", "Awaghad", "Madhuban Colony,Old Jalna", "Jalna", "MH", "43120311", "9422329006");
+        Assert.assertEquals("java.lang.Exception: zip must be 6 digit long", result.toString());
+    }
+
+    @Test
+    public void givenPersonInformation_WithMobileNumberDontFollowRules_ShouldThrowException() throws Exception {
+        AddressbookImplementation obj = new AddressbookImplementation();
+        obj.readAddressBook("/home/admin1/IdeaProjects/AddressBook/src/main/resources/MH.json");
+        Object result = obj.addPerson("Sangita", "Awaghad", "Madhuban Colony,Old Jalna", "Jalna", "MH", "431203", "942232900611");
+        Assert.assertEquals("java.lang.Exception: mobile number must be 10 digit long", result.toString());
     }
 }
